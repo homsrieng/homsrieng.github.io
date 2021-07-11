d52a7c9ade279438d4c5d2531ecab19de273e344
homsrieng <srieng.planb@gmail.com>
2021-07-11 23:41:13 +0700

    Update skill chart

--- js/main.js
+++ js/main.js
@@ -289,3 +289,111 @@
 	});		
 
 })(jQuery);
+
+// SPIDER CHART
+Highcharts.chart('container', {
+
+    chart: {
+        polar: true,
+        type: 'line'
+    },
+
+    // accessibility: {
+    //     description: 'A spiderweb chart compares the allocated budget against actual spending within an organization. The spider chart has six spokes. Each spoke represents one of the 6 departments within the organization: sales, marketing, development, customer support, information technology and administration. The chart is interactive, and each data point is displayed upon hovering. The chart clearly shows that 4 of the 6 departments have overspent their budget with Marketing responsible for the greatest overspend of $20,000. The allocated budget and actual spending data points for each department are as follows: Sales. Budget equals $43,000; spending equals $50,000. Marketing. Budget equals $19,000; spending equals $39,000. Development. Budget equals $60,000; spending equals $42,000. Customer support. Budget equals $35,000; spending equals $31,000. Information technology. Budget equals $17,000; spending equals $26,000. Administration. Budget equals $10,000; spending equals $14,000.'
+    // },
+
+    // title: {
+    //     text: 'Budget vs spending',
+    //     x: -80
+    // },
+
+    pane: {
+        size: '80%'
+    },
+
+    xAxis: {
+        categories: [
+			'Html',
+			'Css',
+			'Jquery',
+			'Php',
+			'Codeignitor',
+			'Laravel',
+			'Crudbooster',
+			'Java',
+			'Kotlin',
+			'Vue.Js',
+			'VB.Net',
+			'C#',
+			'MYSQL',
+			'SQL Server',
+			'Firebase',
+			'Onesignal',
+			'ASP.NET',
+			'JSP'
+		],
+        tickmarkPlacement: 'on',
+        lineWidth: 0
+    },
+
+    yAxis: {
+        gridLineInterpolation: 'polygon',
+        lineWidth: 0,
+        min: 0
+    },
+
+    tooltip: {
+        shared: false,
+        pointFormat: '<span style="color:{series.color}">{series.name}: <b>{point.y}%</b><br/>'
+    },
+
+    legend: {
+        align: 'right',
+        verticalAlign: 'middle',
+        layout: 'vertical'
+    },
+
+    series: [{
+        name: '',
+        data: [
+			80,//html
+			70,//css
+			70,//jquery
+			80,//php
+			90,//ci
+			90,//Laravel
+			80,//Crudbooster	
+			90,//Java
+			80,//Kotlin
+			70,//Vue.Js
+			75,//VB.Net,
+			65,//C#,
+			86,//MYSQL,
+			75,//SQL Server
+			70,//Firebase
+			70,//Onesignal
+			50,//ASP.Net,
+			60,//JSP
+		],
+        pointPlacement: 'on'
+    }],
+
+    responsive: {
+        rules: [{
+            condition: {
+                maxWidth: 500
+            },
+            chartOptions: {
+                legend: {
+                    align: 'center',
+                    verticalAlign: 'bottom',
+                    layout: 'horizontal'
+                },
+                pane: {
+                    size: '70%'
+                }
+            }
+        }]
+    }
+
+});
